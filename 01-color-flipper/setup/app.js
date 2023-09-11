965a773c371f09b4b81fe0d47a257e6be08ea4c8
Harsh <harsh@Harshs-MacBook-Air.local>
2023-09-11 22:10:00 +0530

    first js freecodecamp project

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const colors1 = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  const color = colors[randomInt()];
+
+  document.body.style.backgroundColor = color;
+  colors1.textContent = color;
+});
+
+function randomInt() {
+  return Math.floor(colors.length * Math.random());
+}
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const colorText = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[randomInt()];
+  }
+
+  colorText.textContent = color;
+  document.body.style.backgroundColor = color;
+});
+
+function randomInt() {
+  return Math.floor(hex.length * Math.random());
+}
